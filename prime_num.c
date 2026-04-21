c0523866dff03a84a405abee3cdf43fbec38a0c7
yashashvibisenbatch2024-cmd <yashashvi.bisen.batch2024@sitnagpur.siu.edu.in>
2026-04-21 12:06:18 +0530

    Add files via upload

---
+++ prime_num.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        printf("Not Prime");
+        return 0;
+    }
+
+    for (i = 2; i < n; i++) {
+        if (n % i == 0) {
+            printf("Not Prime");
+            return 0;
+        }
+    }
+
+    printf("Prime");
+    return 0;
+}
+
